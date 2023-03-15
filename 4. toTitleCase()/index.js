180e73aee5f9fe763993329da0177b701430487b
Jack <li.jack0707@gmail.com>
2023-03-15 14:59:35 -0400

    chore: initial commit

---
+++ 4. toTitleCase()/index.js
@@ -0,0 +1,36 @@
+/* toTitleCase
+Write a function that will capitalize every word in a sentence.  
+
+Example Input: "everything, everywhere, all at once"
+Example Output: "Everything, Everywhere, All At Once"
+*/
+
+/* 
+First, write a function that takes in one word and 
+capitalizes the first letter of that word.
+
+Example Input: "scrimba"
+Example Output: "Scrimba"
+
+Hint: Trying using slice() and .toUpperCase()
+*/
+
+function capitalizeWord(word){
+    return word.charAt(0).toUpperCase() + word.slice(1, word.length)
+}
+
+/* 
+Now write a function that capitalizes every word in a sentence. 
+How can you reuse the function you just wrote? 
+*/ 
+
+function toTitleCase(str){
+    const newStr = str.split(" ")
+                      .map(item =>capitalizeWord(item))
+                      .join(" ")
+    return newStr
+}
+
+// Test your functions
+console.log(capitalizeWord("pumpkin"));
+console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
