e969f5b63efa5b5939fdac55b7f499d66a3be624
Jack <li.jack0707@gmail.com>
2023-03-16 13:54:36 -0400

    chore: initial commit

---
+++ 12. Array Duplicate Remover/index.js
@@ -0,0 +1,45 @@
+/* Chef Mario's Recipe Book 
+Chef Mario was in the middle of writing his cookbook masterpiece
+when he spilled coffee on his keyboard! Now all his recipes have repeat
+ingredients.
+
+Help save Chef Mario's cookbook by writing a function that takes in an array 
+and returns a new array with all the duplicates removed. 
+
+Example input: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops", "🦄 unicorn", "🍭 lollipops"];
+Example output: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops"];
+*/ 
+
+const eggScrambleRecipe = [
+            "🥓 bacon",
+            "🥓 bacon", 
+            "🍳 eggs",
+            "🫑 green peppers",
+            "🧀 cheese",
+            "🌶️ hot sauce",
+            "🥓 bacon",
+            "🥦 broccoli", 
+            "🧀 cheese",
+            "🥦 broccoli", 
+            "🌶️ hot sauce"
+        ]
+
+function removeDupesFromArray(arr){
+    //loop through the array    
+    
+    let newArr = []
+    
+    newArr = arr.filter(item => {
+        //check current item against existing items in the array
+        //if not, create an item
+        if(!newArr.includes(item)){
+            // use .push to add 
+            
+            return newArr.push(item)
+        }
+    })
+    //return modified array
+    return newArr   
+}
+
+console.log(removeDupesFromArray(eggScrambleRecipe));
