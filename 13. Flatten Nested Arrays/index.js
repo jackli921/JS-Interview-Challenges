6685530278d5ccd7b348c9267e5b64cbe5096acf
Jack <li.jack0707@gmail.com>
2023-03-16 14:22:56 -0400

    chore: initial commit

---
+++ 13. Flatten Nested Arrays/index.js
@@ -0,0 +1,50 @@
+
+/* 
+Scrimba mascot Pumpkin has won the grand prize at an international 
+cat show. Below are Pumpkin's scores from the judges, as well as all the 
+prizes he's won. In all the excitement of victory,
+they've become a jumbled mess of nested arrays. Let's 
+help Pumpkin by sorting it out. 
+
+Write a function to flatten nested arrays of strings or
+numbers into a single array. There's a method
+for this, but pratice both doing it manually and using the method. 
+
+Example input: [1, [4,5], [4,7,6,4], 3, 5]
+Example output: [1, 4, 5, 4, 7, 6, 4, 3, 5]
+*/
+
+const kittyScores = [
+    [39, 99, 76], 89, 98, [87, 56, 90], 
+    [96, 95], 40, 78, 50, [63]
+];
+
+const kittyPrizes = [
+    ["💰", "🐟", "🐟"], "🏆", "💐", "💵", ["💵", "🏆"],
+    ["🐟","💐", "💐"], "💵", "💵", ["🐟"], "🐟"
+];
+
+function flatten(arr){
+    //loop through the array
+    //if the item is an array,
+        //spread the items inside and push them to the parent array
+    //if not, do nothing
+    let newArr = []
+    
+    arr.forEach(item =>{
+        //forEach doesn't return an array but performs an fx on each array element
+        //map will always return an array of the same length as the original (not ideal)
+        if(Array.isArray(item)){
+           //use spread operator to acess the values inside the array 
+           //use push to add the values 
+           newArr.push(...item) 
+        }
+        else{
+            item
+        }
+    })
+    return newArr
+}
+
+console.log(flatten(kittyPrizes));
+console.log(flatten(kittyScores));
